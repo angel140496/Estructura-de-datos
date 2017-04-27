1f71f195a34e2bf8c7090ac9c5ba718bc9cbdca5
angel140496 <jtorres24@ucol.mx>
2017-04-27 00:14:45 -0500

    Add files via upload

---
+++ Matriz/Matriz/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Matriz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 112);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Llenar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mostrar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 107);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Llenar por Columna";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(24, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 47);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Llenar por Rengol inverso";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(24, 232);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 48);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Mostrar Diagonal Principal";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(124, 178);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 48);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Mostrar Diagonal Secundaria";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(124, 232);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 48);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Mostrar Diagonal Arriba";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(205, 178);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 48);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Mostrar Diagonal Abajo";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(205, 232);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 48);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Girar";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 304);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Matriz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Matriz/Matriz/Matriz2.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    class Matriz2
+    {
+        private string vCadena;
+        private int _aux;
+        public int[,] _matriz;
+        int _tamaño;
+
+        public Matriz2(int Tam)
+        {
+            _tamaño = Tam;
+            _matriz= new int[_tamaño,_tamaño];
+        }
+
+
+        public void llenar()
+        {
+            _aux = 1;
+            for(int i = 0; i < _tamaño; i++)
+            {
+                for (int j = 0; j < _tamaño; j++)
+                {
+                    
+                    _matriz[i,j] = (_aux++);
+                }
+            }
+
+        }
+
+        public void llenarxRegInv()
+        {
+            _aux = (_tamaño*_tamaño);
+            for (int i = 0; i < _tamaño; i++)
+            {
+                for (int j = 0; j < _tamaño; j++)
+                {
+
+                    _matriz[i, j] = (_aux--);
+                }
+            }
+
+        }
+
+        public void llenarXCol()
+        {
+            _aux = 1;
+            for (int i = 0; i < _tamaño; i++)
+            {
+                for (int j = 0; j < _tamaño; j++)
+                {
+
+                    _matriz[j, i] = (_aux++);
+                }
+            }
+        
+        }
+
+        public string mostrarDiagPrin()
+        {
+
+            vCadena = "";
+            int i = 0; 
+
+                for (int j = 0; j < _tamaño;)
+                {
+
+                    vCadena += _matriz[i,j].ToString("000") + " ";
+                i++;
+                    j++;
+                    
+                }
+                
+           
+            return vCadena;
+
+        }
+
+        public string mostrarDiagSec()
+        {
+
+            vCadena = "";
+            int i = 3;
+
+            for (int j = 0; j < _tamaño;)
+            {
+
+                vCadena += _matriz[i, j].ToString("000") + " ";
+                i--;
+                j++;
+
+            }
+
+
+            return vCadena;
+
+        }
+
+        public string mostrarDiagArriba()
+        {
+
+            vCadena = "";
+            int i = 1;
+
+            for (int j = (i + 1); j < _tamaño;)
+            {
+
+                vCadena += _matriz[i, j].ToString("000") + " ";
+                i++;
+                j++;
+
+            }
+
+
+            return vCadena;
+
+        }
+
+        public void mostrarDiagAbajo()
+        {
+
+            vCadena = "";
+            int i;
+            for (i=1; i < _tamaño;)
+            {
+                for (int j = 0; j < i;)
+                {
+
+                    vCadena += _matriz[i, j].ToString("000") + " ";
+                    i++;
+                    j++;
+
+                }
+
+            }
+           
+
+        }
+
+        public void mostrarGirDiagPrin()
+        {
+
+            vCadena = "";
+            int aux;
+
+            for (int i = 1; i < _matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    aux = _matriz[i, j];
+                    _matriz[i, j] = _matriz[j,i];
+                    _matriz[j,i] = aux;
+                }
+            }
+        }
+
+
+        public string mostrar()
+        {
+            vCadena = "";
+           for (int i = 0; i < _tamaño; i++)
+                {
+             
+                for (int j = 0; j < _tamaño; j++)
+                    {
+
+                    vCadena += _matriz[i, j].ToString("000")+ " ";
+                    }
+                vCadena += Environment.NewLine;
+                }
+            vCadena += "\n\n";
+            return vCadena;
+        }
+
+
+
+    }
+}
